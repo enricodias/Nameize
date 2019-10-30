ad5e4bbe365b160125d086d2564fcca2ac943768
enricodias <enricodias@gmail.com>
2019-10-30 14:52:06 -0300

    Update Nameize.php
    Add comments.
    Update code style to PSR-12

--- src/Nameize.php
+++ src/Nameize.php
@@ -2,31 +2,70 @@
 
 namespace enricodias;
 
+/**
+ * Nameize
+ * 
+ * A simple class to correctly capitalize full names.
+ * 
+ * @author Enrico Dias <enrico@enricodias.com>
+ * @link   https://github.com/enricodias/nameize Github repository.
+ */
 class Nameize
 {	
+	/**
+	 * Minimum word length in which a word will have its first character capitalized.
+	 *
+	 * @var int
+	 */
 	private $_minLength = 4;
+
+	/**
+	 * An array of allowed characters.
+	 * 
+	 * Those characters sinalizes that the next letter of a word should be in upper case.
+	 *
+	 * @var array
+	 */
 	private $_allowedCharacters = array(' ', "'", '-');
 	
-	public function __construct($allowedCharacters = null) {
-		
+	/**
+	 * Create a new Nameize instance and set allowed characters.
+	 *
+	 * @see    Nameize::$_allowedCharacters Definition of allowed characters.
+	 * @param  string|array $allowedCharacters A single or an array of characters to be set as allowed characters.
+	 * @return Nameize
+	 */
+	public function __construct($allowedCharacters = null)
+	{
 		if ($allowedCharacters !== null) $this->setAllowedCharacters($allowedCharacters);
 
 		return $this;
-
 	}
 
-	public function minLength($length) {
-
+	/**
+	 * Sets the minimum word length.
+	 *
+	 * @see    Nameize::$_minLength Definition of minimum word length.
+	 * @param  int $length Minimum word length, must be between 1 and 5.
+	 * @return Nameize
+	 */
+	public function minLength($length)
+	{
 		if (!is_int($length) || $length < 1 || $length > 5) return;
 
 		$this->_minLength = $length;
 
 		return $this;
-
 	}
 
-	public function name($name) {
-
+	/**
+	 * Correctly capitalize a full name.
+	 *
+	 * @param  string $name Name to be capitalized.
+	 * @return string
+	 */
+	public function name($name)
+	{
 		$string = mb_strtolower($name, 'UTF-8');
 		
 		if (is_array($this->_allowedCharacters) && !empty($this->_allowedCharacters)) {
@@ -60,26 +99,35 @@ class Nameize
 		$this->_processedName = self::ucFirst($string);
 
 		return $this->_processedName;
-		
 	}
 	
-	public function setAllowedCharacters($characters) {
-
+	/**
+	 * Set one or more allowed characters.
+	 *
+	 * @see    Nameize::$_allowedCharacters Definition of allowed characters.
+	 * @param  string|array $characters A single character or an array of characters.
+	 * @return void
+	 */
+	public function setAllowedCharacters($characters)
+	{
 		if (!is_array($characters)) $characters = array($characters);
 
 		$characters[] = ' '; // space is always used
 		$characters = array_unique($characters);
 
 		$this->_allowedCharacters = $characters;
-
 	}
 
-	private static function ucFirst($string) {
-		
+	/**
+	 * Make a string's first character uppercase using mbstring.
+	 *
+	 * @param  string $string The input string.
+	 * @return string
+	 */
+	private static function ucFirst($string)
+	{
 		$string = mb_strtoupper(mb_substr($string, 0, 1)).mb_substr($string, 1);
 		
 		return $string;
-	
 	}
-
 }
